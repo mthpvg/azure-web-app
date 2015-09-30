4a5a3e770ee164d3a576d805978192d4496d3491
mthpvg <mth.pvg@gmail.com>
2015-09-30 14:59:02 +0200

    windows cpu

--- routes/webpages/index.js
+++ routes/webpages/index.js
@@ -23,6 +23,9 @@ router.get('/', function (req, res) {
   var loadCPU = [];
   var loadavg = os.loadavg();
   var hostname = os.hostname();
+  var type = os.type();
+  var platform = os.platform();
+  var arch = os.arch();
   
   for (var i = 0; i < loadavg.length; i++) {
     loadCPU.push(parseFloat((1.0 * loadavg[i] / nbCPU * 100).toFixed(2)));
@@ -30,25 +33,57 @@ router.get('/', function (req, res) {
   
   var data = {
     "hostname": hostname,
+    "type": type,
+    "platform": platform,
+    "arch": arch,
     "nbCPU": nbCPU,
     "loadCPU": loadCPU,
+    "loadavg": loadavg,
     "loadRAM": parseFloat(loadRAM.toFixed(2)),
   };
   
   var d = new Date();
   data.UTCdate = d.toISOString();
+  console.log(platform);
+  if (platform === "linux") {
+    
+    res.render('index', {
+      scripts: [
+        GLOBAL.SCRIPTS.JQUERY,
+        GLOBAL.SCRIPTS.BOOTSTRAP
+      ],
+      links: [
+        {rel: 'stylesheet', href: GLOBAL.LINKS.BOOTSTRAP},
+        {rel: 'stylesheet', href: '/css/index.css'}
+      ],
+      data: data
+    });
+  } else {
+    var wincpu = require('windows-cpu');
+    wincpu.totalLoad(function(error, results) {
+      if(error) {
+        return console.log(error);
+      } else {
+        data.wincpu = results;
+      }
+      
+      res.render('index', {
+        scripts: [
+          GLOBAL.SCRIPTS.JQUERY,
+          GLOBAL.SCRIPTS.BOOTSTRAP
+        ],
+        links: [
+          {rel: 'stylesheet', href: GLOBAL.LINKS.BOOTSTRAP},
+          {rel: 'stylesheet', href: '/css/index.css'}
+        ],
+        data: data
+      });
+      
+      
+    });
+    
+  }
   
-  res.render('index', {
-    scripts: [
-      GLOBAL.SCRIPTS.JQUERY,
-      GLOBAL.SCRIPTS.BOOTSTRAP
-    ],
-    links: [
-      {rel: 'stylesheet', href: GLOBAL.LINKS.BOOTSTRAP},
-      {rel: 'stylesheet', href: '/css/index.css'}
-    ],
-    data: data
-  });
 });
 //-----------------------------------------------------------------------------
 //									                  EXPORTS

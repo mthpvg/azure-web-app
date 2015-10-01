ecaaa8e7ef6b24504a5dc106bb3657b70eb181a6
mthpvg <mth.pvg@gmail.com>
2015-10-01 09:31:32 +0200

    loadCPU for win and linux

--- routes/webpages/index.js
+++ routes/webpages/index.js
@@ -20,7 +20,7 @@ var hostUUID = uuid.v4();
 //-----------------------------------------------------------------------------
 router.get('/', function (req, res) {
   
-
+  
   var nbCPU = os.cpus().length;
   var loadRAM = 1.0 * os.freemem() / os.totalmem() * 100;
   var loadCPU = [];
@@ -44,7 +44,7 @@ router.get('/', function (req, res) {
     "platform": platform,
     "arch": arch,
     "nbCPU": nbCPU,
-    "loadCPU": loadCPU,
+    //"loadCPU": loadCPU,
     "loadRAM": parseFloat(loadRAM.toFixed(2)),
     "networkInterfaces": networkInterfaces,
     "cpus": cpus,
@@ -54,17 +54,68 @@ router.get('/', function (req, res) {
   var d = new Date();
   data.UTCdate = d.toISOString();
   
-  res.render('index', {
-    scripts: [
-      GLOBAL.SCRIPTS.JQUERY,
-      GLOBAL.SCRIPTS.BOOTSTRAP
-    ],
-    links: [
-      {rel: 'stylesheet', href: GLOBAL.LINKS.BOOTSTRAP},
-      {rel: 'stylesheet', href: '/css/index.css'}
-    ],
-    data: data
-  });
+  function cpuAverage() {
+    
+    //Initialise sum of idle and time of cores and fetch CPU info
+    var totalIdle = 0, totalTick = 0;
+    var cpus = os.cpus();
+    
+    //Loop through CPU cores
+    for(var i = 0, len = cpus.length; i < len; i++) {
+      
+      //Select CPU core
+      var cpu = cpus[i];
+      
+      //Total up the time in the cores tick
+      var type;
+      for(type in cpu.times) {
+        totalTick += cpu.times[type];
+      }     
+      
+      //Total up the idle time of the core
+      totalIdle += cpu.times.idle;
+    }
+    
+    //Return the average Idle and Tick times
+    return {idle: totalIdle / cpus.length,  total: totalTick / cpus.length};
+  }
+  
+  
+  //Grab first CPU Measure
+  var startMeasure = cpuAverage();
+  
+  //Set delay for second Measure
+  setTimeout(function() { 
+    
+    //Grab second Measure
+    var endMeasure = cpuAverage(); 
+    
+    //Calculate the difference in idle and total time between the measures
+    var idleDifference = endMeasure.idle - startMeasure.idle;
+    var totalDifference = endMeasure.total - startMeasure.total;
+    
+    //Calculate the average percentage CPU usage
+    var percentageCPU = 100 - ~~(100 * idleDifference / totalDifference);
+    data.loadCPU = percentageCPU;
+    //Output result to console
+    console.log(percentageCPU + "% CPU Usage.");
+    
+    res.render('index', {
+      scripts: [
+        GLOBAL.SCRIPTS.JQUERY,
+        GLOBAL.SCRIPTS.BOOTSTRAP
+      ],
+      links: [
+        {rel: 'stylesheet', href: GLOBAL.LINKS.BOOTSTRAP},
+        {rel: 'stylesheet', href: '/css/index.css'}
+      ],
+      data: data
+    });
+    
+  }, 100);
+  
+  
+
 });
 //-----------------------------------------------------------------------------
 //									                  EXPORTS
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ logger = require('morgan'),
 bodyParser = require('body-parser'),
 cookieParser = require('cookie-parser'),
 expressSession = require('express-session'),
+os = require('os'),
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -CONFIG
 GLOB = require('./config/GLOBAL'),
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -ROUTES
@@ -20,6 +21,63 @@ app = express();
 //-----------------------------------------------------------------------------
 //                                     PROGRAM
 //-----------------------------------------------------------------------------
+function cpuAverage() {
+	
+	//Initialise sum of idle and time of cores and fetch CPU info
+	var totalIdle = 0, totalTick = 0;
+	var cpus = os.cpus();
+	
+	//Loop through CPU cores
+	for(var i = 0, len = cpus.length; i < len; i++) {
+		
+		//Select CPU core
+		var cpu = cpus[i];
+		
+		//Total up the time in the cores tick
+		var type;
+		for(type in cpu.times) {
+			totalTick += cpu.times[type];
+		}     
+		
+		//Total up the idle time of the core
+		totalIdle += cpu.times.idle;
+	}
+	
+	//Return the average Idle and Tick times
+	return {idle: totalIdle / cpus.length,  total: totalTick / cpus.length};
+}
+
+function plop() {
+	//Grab first CPU Measure
+	var startMeasure = cpuAverage();
+	
+	//Set delay for second Measure
+	setTimeout(function() { 
+		
+		//Grab second Measure
+		var endMeasure = cpuAverage(); 
+		
+		//Calculate the difference in idle and total time between the measures
+		var idleDifference = endMeasure.idle - startMeasure.idle;
+		var totalDifference = endMeasure.total - startMeasure.total;
+		
+		//Calculate the average percentage CPU usage
+		var percentageCPU = 100 - ~~(100 * idleDifference / totalDifference);
+		
+		//Output result to console
+		console.log(percentageCPU + "% CPU Usage.");
+		
+	}, 100);
+	
+}
+
+setInterval(plop, 10*1000);
+
+
+
+
+
+
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -JADE
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -39,7 +97,7 @@ app.use(expressSession(
 app.use(logger('dev'));
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - BODY PARSER
 app.use(bodyParser.urlencoded({
-  extended: true
+	extended: true
 }));
 app.use(bodyParser.json());
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -STYLUS
